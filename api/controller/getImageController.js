f1786ef33c88527eaeab70cb1a9e16d871c7aa91
Lenny Andreu <landreuhs.29@gmail.com>
2021-07-26 11:54:46 -0400

    Savepoint 6

--- api/controller/getImageController.js
+++ api/controller/getImageController.js
@@ -5,8 +5,13 @@ const getImageService = require("../service/getImageService");
 /* GET home page. */
 router.get('/:Bucket/:Key', async (req, res, next) => {
     const { Bucket, Key } = req.params
+    const { raw } = req.query
     const response = await getImageService.get(Bucket, Key)
 
+    if(raw !== undefined){
+        console.log(response);
+        return next();
+    }
     res.status(200).send({ url: response });
 });
 
